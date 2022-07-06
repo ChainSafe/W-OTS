d78141d3718f24c056dc2c5855c547899004fc76
noot <36753753+noot@users.noreply.github.com>
2022-07-06 02:32:20 -0400

    update default params to have generic hashers (#16)
    * update default params
    
    * cleanup
    
    * address comment

--- src/security.rs
+++ src/security.rs
@@ -1,6 +1,6 @@
 use std::convert::From;
 
-use crate::hasher::{Blake2bHasher, Sha3_224Hasher, Sha3_256Hasher};
+use crate::hasher::{Blake2bHasher, Hasher, Sha3_224Hasher, Sha3_256Hasher};
 use crate::params::{Params, WotsError};
 
 #[derive(Debug, Clone)]
@@ -39,38 +39,62 @@ impl From<&ParamsEncoding> for u8 {
     }
 }
 
-pub fn level_0_params() -> Params<Blake2bHasher, Sha3_224Hasher> {
-    Params::<Blake2bHasher, Sha3_224Hasher>::new(ParamsEncoding::Level0)
-        .expect("instantiating level0 params should not fail")
+impl<PRFH: Hasher + Clone, MSGH: Hasher + Clone> From<&ParamsEncoding> for Params<PRFH, MSGH> {
+    fn from(item: &ParamsEncoding) -> Self {
+        match item {
+            ParamsEncoding::Custom => consensus_params(), // TODO
+            _ => Params::new(item.clone()).expect("instantiating level0 params should not fail"),
+        }
+    }
 }
 
-pub fn level_1_params() -> Params<Blake2bHasher, Sha3_224Hasher> {
-    Params::<Blake2bHasher, Sha3_224Hasher>::new(ParamsEncoding::Level1)
-        .expect("instantiating level1 params should not fail")
+pub fn level_0_params<PRFH: Hasher + Clone, MSGH: Hasher + Clone>() -> Params<PRFH, MSGH> {
+    Params::new(ParamsEncoding::Level0).expect("instantiating level0 params should not fail")
 }
 
-pub fn level_2_params() -> Params<Blake2bHasher, Sha3_224Hasher> {
-    Params::<Blake2bHasher, Sha3_224Hasher>::new(ParamsEncoding::Level2)
-        .expect("instantiating level2 params should not fail")
+pub fn level_1_params<PRFH: Hasher + Clone, MSGH: Hasher + Clone>() -> Params<PRFH, MSGH> {
+    Params::new(ParamsEncoding::Level1).expect("instantiating level1 params should not fail")
 }
 
-pub fn level_3_params() -> Params<Blake2bHasher, Sha3_224Hasher> {
-    Params::<Blake2bHasher, Sha3_224Hasher>::new(ParamsEncoding::Level3)
-        .expect("instantiating level3 params should not fail")
+pub fn level_2_params<PRFH: Hasher + Clone, MSGH: Hasher + Clone>() -> Params<PRFH, MSGH> {
+    Params::new(ParamsEncoding::Level2).expect("instantiating level2 params should not fail")
 }
 
-pub fn consensus_params() -> Params<Blake2bHasher, Sha3_256Hasher> {
-    Params::<Blake2bHasher, Sha3_256Hasher>::new(ParamsEncoding::Consensus)
-        .expect("instantiating consensus params should not fail")
+pub fn level_3_params<PRFH: Hasher + Clone, MSGH: Hasher + Clone>() -> Params<PRFH, MSGH> {
+    Params::new(ParamsEncoding::Level3).expect("instantiating level3 params should not fail")
+}
+
+pub fn consensus_params<PRFH: Hasher + Clone, MSGH: Hasher + Clone>() -> Params<PRFH, MSGH> {
+    Params::new(ParamsEncoding::Consensus).expect("instantiating consensus params should not fail")
 }
 
 pub fn verify(msg: &[u8], signature: &[u8], public_key: &[u8]) -> Result<(), WotsError> {
     match ParamsEncoding::from(signature[0]) {
-        ParamsEncoding::Level0 => level_0_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Level1 => level_1_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Level2 => level_2_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Level3 => level_3_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Consensus => consensus_params().verify(msg, &signature[1..], public_key),
+        ParamsEncoding::Level0 => level_0_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Level1 => level_1_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Level2 => level_2_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Level3 => level_3_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Consensus => consensus_params::<Blake2bHasher, Sha3_256Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
         _ => Err(WotsError::InvalidParamsEncodingType),
     }
 }
@@ -82,10 +106,26 @@ pub fn verify_no_consensus(
     public_key: &[u8],
 ) -> Result<(), WotsError> {
     match ParamsEncoding::from(signature[0]) {
-        ParamsEncoding::Level0 => level_0_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Level1 => level_1_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Level2 => level_2_params().verify(msg, &signature[1..], public_key),
-        ParamsEncoding::Level3 => level_3_params().verify(msg, &signature[1..], public_key),
+        ParamsEncoding::Level0 => level_0_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Level1 => level_1_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Level2 => level_2_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
+        ParamsEncoding::Level3 => level_3_params::<Blake2bHasher, Sha3_224Hasher>().verify(
+            msg,
+            &signature[1..],
+            public_key,
+        ),
         _ => Err(WotsError::InvalidParamsEncodingType),
     }
 }
@@ -100,30 +140,30 @@ mod tests {
 
     #[test]
     fn params_test() {
-        let params = security::level_0_params();
+        let params = security::level_0_params::<Blake2bHasher, Sha3_224Hasher>();
         assert_eq!(params.n, 20);
         assert_eq!(params.m, 24);
 
-        let params = security::level_1_params();
+        let params = security::level_1_params::<Blake2bHasher, Sha3_224Hasher>();
         assert_eq!(params.n, 24);
         assert_eq!(params.m, 24);
 
-        let params = security::level_2_params();
+        let params = security::level_2_params::<Blake2bHasher, Sha3_224Hasher>();
         assert_eq!(params.n, 28);
         assert_eq!(params.m, 24);
 
-        let params = security::level_3_params();
+        let params = security::level_3_params::<Blake2bHasher, Sha3_224Hasher>();
         assert_eq!(params.n, 32);
         assert_eq!(params.m, 24);
 
-        let params = security::consensus_params();
+        let params = security::consensus_params::<Blake2bHasher, Sha3_256Hasher>();
         assert_eq!(params.n, 32);
         assert_eq!(params.m, 32);
     }
 
     #[test]
     fn verify_consensus_params_should_fail() {
-        let params = security::consensus_params();
+        let params = security::consensus_params::<Blake2bHasher, Sha3_256Hasher>();
         let sig_size = (params.n * params.total) + 1 + SEED_SIZE;
         let mut key = Key::<Blake2bHasher, Sha3_256Hasher>::new(params).unwrap();
         key.generate().unwrap();
@@ -153,15 +193,14 @@ mod tests {
 
     #[test]
     fn verify_test_generate() {
-        let params = security::level_3_params();
+        let params = security::consensus_params();
         let sig_size = (params.n * params.total) + 1 + SEED_SIZE;
-        let mut key = Key::<Blake2bHasher, Sha3_224Hasher>::new(params).unwrap();
+        let mut key = Key::<Blake2bHasher, Sha3_256Hasher>::new(params).unwrap();
         key.generate().unwrap();
 
         let msg = vec![99u8; MAX_MSG_SIZE];
         let res = key.sign(&msg).unwrap();
         assert_eq!(res.len(), sig_size);
-
         verify(&msg, &res, &key.public_key).unwrap();
     }
 }
